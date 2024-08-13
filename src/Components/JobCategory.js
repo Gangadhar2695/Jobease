0f641299b92daed3563822d7cee18aa21d4b4842
Gangadhar <gangadhar.veeravalli09@gmail.com>
2024-08-13 21:01:18 +0530

    items length changed

--- src/Components/JobCategory.js
+++ src/Components/JobCategory.js
@@ -27,7 +27,7 @@ const JobCategory = () => {
   };
 
   const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 5;
+  const itemsPerPage = 10;
 
   const filteredData = data.filter(item => item.Category === category);
   // console.log('filterdata', filteredData)
